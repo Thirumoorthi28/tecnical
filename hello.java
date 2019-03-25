85fe8e7cf279dd347db1aaed3dd59933e62f8929
Thirumoorthi28 <48052999+Thirumoorthi28@users.noreply.github.com>
2019-03-25 16:14:09 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Leapyear{
+    public static void main(String args[]){
+        int a,i;
+        Scanner s=new Scanner(System.in);
+        i=s.nextInt();
+        System.out.println("Enter The value :");
+        for(a=1;a<=i;a++)
+        {
+            System.out.println("HELLO");
+            
+        }
+        
+    }
+}
